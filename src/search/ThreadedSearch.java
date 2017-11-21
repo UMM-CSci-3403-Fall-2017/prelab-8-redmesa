11b78b739f198448e6539e9da5da23fd7049064b
root <root@DESKTOP-FK3SQFB.localdomain>
2017-11-21 11:26:04 -0600

    Lab 8-prelab was short, creadted a thread handler, then implemented run to run as long at the list is advaible, then joins was created t join the threds to create an answer.

--- src/search/ThreadedSearch.java
+++ src/search/ThreadedSearch.java
@@ -21,41 +21,38 @@ public class ThreadedSearch<T> implements Runnable {
     this.answer=answer;
   }
 
-  /**
-  * Searches `list` in parallel using `numThreads` threads.
-  *
-  * You can assume that the list size is divisible by `numThreads`
-  */
   public boolean parSearch(int numThreads, T target, ArrayList<T> list) throws InterruptedException {
-    /*
-    * First construct an instance of the `Answer` inner class. This will
-    * be how the threads you're about to create will "communicate". They
-    * will all have access to this one shared instance of `Answer`, where
-    * they can update the `answer` field inside that instance.
-    *
-    * Then construct `numThreads` instances of this class (`ThreadedSearch`)
-    * using the 5 argument constructor for the class. You'll hand each of
-    * them the same `target`, `list`, and `answer`. What will be different
-    * about each instance is their `begin` and `end` values, which you'll
-    * use to give each instance the "job" of searching a different segment
-    * of the list. If, for example, the list has length 100 and you have
-    * 4 threads, you would give the four threads the ranges [0, 25), [25, 50),
-    * [50, 75), and [75, 100) as their sections to search.
-    *
-    * You then construct `numThreads`, each of which is given a different
-    * instance of this class as its `Runnable`. Then start each of those
-    * threads, wait for them to all terminate, and then return the answer
-    * in the shared `Answer` instance.
-    */
-    return false;
+      Answer answer = new Answer();
+
+      Thread[] threads = new Thread[numThreads];
+      int range = list.size() // This is the amount of Threads
+      int start = 0; // Starting at zero
+
+      //Creates new threads
+      for (int i=0; i < numThreads; ++i){
+        threads[i] = new Thread(new ThreadedSearch(target, list, start, start + range, answer));
+        start = start + range;
+        threads[i].start();
+      }
+    //Hangs the threads and and then combines them.
+    for(int i=0;i<numThreads;++i){
+      threads[i].join();
+    }
+    return answer.getAnswer();
   }
 
+  //Looking to see if its done
   public void run() {
-
+    for(int i=0;i<list.size();i++){
+      if(list.get(i).equals(target)){
+        this.answer.setAnswer(true);
+        break;
+      }
+    }
   }
 
   private class Answer {
-    private boolean answer = false;
+    private boolean response = false;
 
     public boolean getAnswer() {
       return answer;
